2c8c6b81585c9600bf341ad8d17d21b4e70cecf2
Kin <kin092002@gmail.com>
2023-12-05 03:16:37 +0700

    Shoes-shop

---
+++ Shoes-Shop/frontend/src/pages/ProfilePage/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import ProfileCard from "~/components/ProfileCard";
+import Header from "../../components/Layout/Header";
+import { useCart } from "../../contexts/CartContext";
+
+export default function ProfilePage() {
+  const { cartItems } = useCart();
+  return (
+    <>
+      <Header cartItems={cartItems} />
+      <ProfileCard />
+      <ToastContainer />
+    </>
+  );
+}
