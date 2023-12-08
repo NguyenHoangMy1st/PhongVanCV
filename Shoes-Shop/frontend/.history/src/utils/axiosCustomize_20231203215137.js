74e8f2abe7ed42056e6bcffd2b4407a4c86452af
Kin <kin092002@gmail.com>
2023-12-08 14:37:31 +0700

    up

--- Shoes-Shop/frontend/.history/src/utils/axiosCustomize_20231203215137.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api_instance = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL=https://pbl6-shoes-shop-production-2639.up.railway.app
-    ,
-});
-
-export default api_instance;
