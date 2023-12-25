f8fc3b7939dd3dbb597752d6ffba88f10389319c
Kin <kin092002@gmail.com>
2023-12-25 18:51:20 +0700

    fix api

--- Shoes-Shop/.history/frontend/src/api/user/apiRegister_20231224223059.js
+++ Shoes-Shop/.history/frontend/src/api/user/apiRegister_20231224223059.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiRegister = {
     postRegister(data) {
-        const url = 'https://pbl6-shoes-shop-production-5c16.up.railway.app/auth/signup';
+        const url = 'https://pbl6-shoes-shop-production-f927.up.railway.app/auth/signup';
         return axios.post(url, data);
     },
 };
