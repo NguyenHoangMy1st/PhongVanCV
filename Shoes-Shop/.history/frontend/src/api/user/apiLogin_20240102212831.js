f5607dfc7fc4c397abcd9d129db49c826e42e872
Kin <kin092002@gmail.com>
2024-03-21 22:11:28 +0700

    update railway

--- Shoes-Shop/.history/frontend/src/api/user/apiLogin_20240102212831.js
+++ Shoes-Shop/.history/frontend/src/api/user/apiLogin_20240102212831.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiLogin = {
     postLogin(data) {
-        const url = 'https://pbl6-shoes-shop-production-28ef.up.railway.app/auth/signin';
+        const url = 'https://pbl6-shoes-shop-production-f0c7.up.railway.app/auth/signin';
         return axios.post(url, data);
     },
 };
--- Shoes-Shop/.history/frontend/src/api/user/apiRegister_20240102212831.js
+++ Shoes-Shop/.history/frontend/src/api/user/apiRegister_20240102212831.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiRegister = {
     postRegister(data) {
-        const url = 'https://pbl6-shoes-shop-production-28ef.up.railway.app/auth/signup';
+        const url = 'https://pbl6-shoes-shop-production-f0c7.up.railway.app/auth/signup';
         return axios.post(url, data);
     },
 };
